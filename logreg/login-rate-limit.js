ea2c524d42e449d9cbd036d7f2576dec840d3471
ChezzyTotty <ahmeterdemserceoglo@gmail.com>
2023-09-23 15:26:14 +0300

    Fixed basic problems

---
+++ logreg/login-rate-limit.js
@@ -0,0 +1,18 @@
+const maxWrongAttemptsByIPperDay = 1;
+const maxConsecutiveFailsByUsernameAndIP = 10;
+
+const limiterSlowBruteByIP = new RateLimiterRedis({
+  storeClient: redisClient,
+  keyPrefix: 'login_fail_ip_per_day',
+  points: maxWrongAttemptsByIPperDay,
+  duration: 60 * 60 * 24,
+  blockDuration: 60 * 60 * 24, // Block for 1 day, if 100 wrong attempts per day
+});
+
+const limiterConsecutiveFailsByUsernameAndIP = new RateLimiterRedis({
+  storeClient: redisClient,
+  keyPrefix: 'login_fail_consecutive_username_and_ip',
+  points: maxConsecutiveFailsByUsernameAndIP,
+  duration: 60 * 60 * 24 * 90, // Store number for 90 days since first fail
+  blockDuration: 60 * 60, // Block for 1 hour
+});
